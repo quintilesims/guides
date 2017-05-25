f5b0fbbfd8b8bdc96085df49d4a8adb9ebac8bcf
Tanner Lake <tanner.lake@gmail.com>
2017-05-25 12:47:51 -0700

    review fixes

--- iterative-walkthrough/deployment-2/main.tf
+++ iterative-walkthrough/deployment-2/main.tf
@@ -10,15 +10,15 @@ resource "layer0_environment" "demo" {
 
 # configure the redis service
 module "redis" {
-  source = "github.com/quintilesims/redis/module"
+  source = "github.com/quintilesims/redis//terraform"
   environment_id = "${layer0_environment.demo.id}"
 }
 
 # configure the guestbook service to use the redis backend 
 module "guestbook" {
-  source         = "github.com/quintilesims/layer0-examples/guestbook/module"
+  source         = "github.com/quintilesims/layer0-examples//guestbook/module"
   environment_id = "${layer0_environment.demo.id}"
-  scale          = 1
+  scale          = 2
   backend_type   = "redis"
   backend_config = "${module.redis.load_balancer_url}:${var.redis_port}"
 }
