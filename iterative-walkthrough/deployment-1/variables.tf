334f34e7c73c19b91ef6390086af13fffa8496a1
Tanner Lake <tanner.lake@gmail.com>
2017-06-05 12:26:30 -0700

    remove iterative-walkthrough/ dir (#14)

--- iterative-walkthrough/deployment-1/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "endpoint" {
-  description = "The endpoint for the layer0 api"
-}
-
-variable "token" {
-  description = "The authentication token for the layer0 api"
-}
--- iterative-walkthrough/deployment-2/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-# Best practice would be to gitignore this file to not share secrets.
-
-#endpoint = "<your Layer0 endpoint>"
-#token = "<your Layer0 token>"
